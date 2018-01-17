0aa322d3e9b7a777d33775e4bd12310efd26b829
L Vicencio <lvicencio@gmail.com>
2018-01-17 02:30:25 -0300

    basico hecho

--- Admin.aspx
+++ Admin.aspx
@@ -6,10 +6,148 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 186px;
+        }
+        .auto-style3 {
+            width: 177px;
+        }
+        .auto-style4 {
+            width: 177px;
+            height: 23px;
+        }
+        .auto-style5 {
+            height: 23px;
+        }
+        .auto-style6 {
+            width: 108px;
+        }
+        .auto-style7 {
+            width: 94px;
+        }
+        .auto-style8 {
+            width: 103px;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
+    
+        <asp:Label ID="lblSaludo" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Panel ID="Panel1" runat="server">
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="margin-left: 616px" Text="Cerrar Session" />
+                    </td>
+                </tr>
+            </table>
+        </asp:Panel>
+        <br />
+        <br />
+        <table class="auto-style1">
+            <tr>
+                <td>
+                    <asp:TextBox ID="txtBuscar" runat="server"></asp:TextBox>
+&nbsp;&nbsp;
+                    <asp:Button ID="btnBuscar" runat="server" OnClick="btnBuscar_Click" Text="Buscar Producto" />
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <asp:Panel ID="Panel2" runat="server">
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Listar Productos" />
+            <br />
+            <asp:GridView ID="GridListadoProducto" runat="server" DataKeyNames="Id_Producto" AutoGenerateColumns="False" OnSelectedIndexChanged="GridListadoProducto_SelectedIndexChanged">
+                <Columns>
+                    <asp:BoundField DataField="Nombre" HeaderText="Nombre" />
+                    <asp:BoundField DataField="Precio" HeaderText="Precio" />
+                    <asp:BoundField DataField="Cantidad" HeaderText="Cantidad" />
+                    <asp:BoundField DataField="Descripcion" HeaderText="Descripción" />
+                    <asp:CommandField HeaderText="Opción" ShowSelectButton="True" />
+                </Columns>
+            </asp:GridView>
+            <br />
+            <br />
+        </asp:Panel>
+        <br />
+        <br />
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style3">Ingresar Producto</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Nombre</td>
+                <td>
+                    <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Descripción</td>
+                <td>
+                    <asp:TextBox ID="txtDescripcion" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Cantidad</td>
+                <td>
+                    <asp:TextBox ID="txtCantidad" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Precio</td>
+                <td class="auto-style5">
+                    <asp:TextBox ID="txtPrecio" runat="server"></asp:TextBox>
+                </td>
+                <td class="auto-style5"></td>
+            </tr>
+            <tr>
+                <td class="auto-style3">
+                    <asp:Label ID="lblMsg" runat="server"></asp:Label>
+                </td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">&nbsp;</td>
+                <td>
+                    &nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <asp:Panel ID="PanelOpciones" runat="server">
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style6">Opciones</td>
+                    <td class="auto-style7">&nbsp;&nbsp;
+                        <asp:Button ID="btnGuardar" runat="server" OnClick="Button3_Click" Text="Guardar" />
+                    </td>
+                    <td class="auto-style8">
+                        <asp:Button ID="btnModificar" runat="server" OnClick="btnModificar_Click" Text="Modificar" />
+                    </td>
+                    <td>&nbsp;<asp:Button ID="btnEliminar" runat="server" OnClick="btnEliminar_Click" Text="Eliminar" />
+                        &nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </asp:Panel>
+        <br />
     
     </div>
     </form>
--- Admin.aspx.cs
+++ Admin.aspx.cs
@@ -4,11 +4,176 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using Entidades;
+using Negocio;
 
 public partial class Admin : System.Web.UI.Page
 {
+
+    public E_Producto objEntProducto = new E_Producto();
+    public N_Productos objNegProducto = new N_Productos();
+
+    public E_Usuario objEntUsuario = new E_Usuario();
+    public N_Usuarios objNegUsuario = new N_Usuarios();
+
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        int sessionrol = Convert.ToInt16(Session["Rol"]);
 
+
+        if (sessionrol != 1)
+        {
+            Response.Write("<script>window.alert('Aviso: No esta autorizado')</script>");
+            Response.Redirect("Login.aspx");
+        }
+        else
+        {
+            lblSaludo.Text = "Bienvenido Sr(a)  " + Session["UserName"].ToString();
+        }
+       // EscondeControl();
+    }
+
+
+    private void Limpiar()
+    {
+        txtCantidad.Text = string.Empty;
+        txtDescripcion.Text = string.Empty;
+        txtNombre.Text = string.Empty;
+        txtPrecio.Text = string.Empty;
+        
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Session.Clear();
+        Response.Redirect("Login.aspx");
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        DataSet ds = new DataSet();
+       
+        ds = objNegProducto.ListadoProductos();
+
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+            GridListadoProducto.DataSource = ds.Tables[0];
+            GridListadoProducto.DataBind();
+            GridListadoProducto.Visible = true;
+        }
+        else
+            Response.Write("<script>window.alert('Aviso: No existen productos')</script>");
+
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        int nGrabados = -1;
+
+        objEntProducto.CantidadProducto = int.Parse(txtCantidad.Text);
+        objEntProducto.DescripcionProducto = txtDescripcion.Text;
+        objEntProducto.NombreProducto = txtNombre.Text;
+        objEntProducto.Precio = int.Parse(txtPrecio.Text);
+
+        nGrabados = objNegProducto.abcProducto("INSERTAR", objEntProducto);
+
+        if (nGrabados != -1)
+        {
+            lblMsg.Text = "Producto Grabado";
+            Limpiar();
+        }
+        else
+        {
+            lblMsg.Text = "Error al Guardar Producto";
+        }
+    }
+
+    protected void btnBuscar_Click(object sender, EventArgs e)
+    {
+        DataSet ds = new DataSet();
+        var NombreProducto = txtBuscar.Text;
+        ds = objNegProducto.BuscarProducto(NombreProducto);
+
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+
+            GridListadoProducto.DataSource = ds.Tables[0];
+            GridListadoProducto.DataBind();
+            GridListadoProducto.Visible = true;
+        }
+    }
+
+    protected void GridListadoProducto_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DataSet ds = new DataSet();
+        objEntProducto.Id_Producto = Convert.ToInt16(GridListadoProducto.DataKeys[GridListadoProducto.SelectedIndex].Value.ToString());
+
+        ds = objNegProducto.SeleccionaProducto(objEntProducto.Id_Producto);
+
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+
+            txtNombre.Text = ds.Tables[0].Rows[0]["Nombre"].ToString();
+            txtDescripcion.Text = ds.Tables[0].Rows[0]["Descripcion"].ToString();
+            txtCantidad.Text = ds.Tables[0].Rows[0]["Cantidad"].ToString();
+            txtPrecio.Text = ds.Tables[0].Rows[0]["Precio"].ToString();
+
+            //EscondeControl();
+            //pnlIngresos.Visible = true;
+            btnGuardar.Visible = false;
+        }
+    }
+
+    protected void btnModificar_Click(object sender, EventArgs e)
+    {
+        int nResultado = -1;
+
+        objEntProducto.Id_Producto = Convert.ToInt16(GridListadoProducto.DataKeys[GridListadoProducto.SelectedIndex].Value.ToString());
+
+        objEntProducto.CantidadProducto = int.Parse(txtCantidad.Text);
+        objEntProducto.DescripcionProducto = txtDescripcion.Text;
+        objEntProducto.NombreProducto = txtNombre.Text;
+        objEntProducto.Precio = int.Parse(txtPrecio.Text);
+        
+        nResultado = objNegProducto.abcProducto("MODIFICAR", objEntProducto);
+
+        if (nResultado != -1)
+        {
+            Response.Write("<script>window.alert('Aviso: El producto fue modificado con exito')</script>");
+            Response.Redirect("Admin.aspx");
+        }
+        else
+        {
+            Response.Write("<script>window.alert('Aviso: NO se modifico el producto')</script>");
+        }
+    }
+
+
+
+
+    protected void btnEliminar_Click(object sender, EventArgs e)
+    {
+        int nResultado = -1;
+
+        objEntProducto.Id_Producto = Convert.ToInt16(GridListadoProducto.DataKeys[GridListadoProducto.SelectedIndex].Value.ToString());
+
+        objEntProducto.CantidadProducto = int.Parse(txtCantidad.Text);
+        objEntProducto.DescripcionProducto = txtDescripcion.Text;
+        objEntProducto.NombreProducto = txtNombre.Text;
+        objEntProducto.Precio = int.Parse(txtPrecio.Text);
+
+        nResultado = objNegProducto.abcProducto("BORRAR", objEntProducto);
+
+        if (nResultado != -1)
+        {
+            Response.Write("<script>window.alert('Aviso: El producto fue modificado con exito')</script>");
+            Response.Redirect("Admin.aspx");
+        }
+        else
+        {
+            Response.Write("<script>window.alert('Aviso: NO se Elimino el producto')</script>");
+        }
     }
 }
